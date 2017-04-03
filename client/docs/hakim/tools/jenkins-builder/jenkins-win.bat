d89fb4159d9709fd5cbd4f2b2bf7056d37980f2b
hakim.hauston <hakim.hauston@9f685826-6130-7644-9a65-f1caa281a29b>
2017-04-03 06:40:49 +0000

    build-jenkins.sh cleanup exit unrecognized syntax
    git-svn-id: https://10.6.0.162/svn/test-build-resources/trunk@3 9f685826-6130-7644-9a65-f1caa281a29b

--- client/docs/hakim/tools/jenkins-builder/jenkins-win.bat
+++ client/docs/hakim/tools/jenkins-builder/jenkins-win.bat
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "jenkins-win.bat executed"
 pushd client\examples\SlotMachine\build
-C:\cygwin64\bin\bash.exe build.sh pre-dev ios_debug ios en_us
-C:\cygwin64\bin\bash.exe build.sh pre-dev ios_debug ios en_us
+C:\cygwin64\bin\bash.exe build-jenkins.sh pre-dev ios_debug ios en_us
 popd
