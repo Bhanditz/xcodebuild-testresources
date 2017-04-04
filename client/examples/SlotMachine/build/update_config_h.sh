3e21bc40e2950556c2f74b376dd35a34f516e2fa
hakim.hauston <hakim.hauston@9f685826-6130-7644-9a65-f1caa281a29b>
2017-04-04 06:10:23 +0000

    remove all checks that causes too many arguments error
    git-svn-id: https://10.6.0.162/svn/test-build-resources/trunk@20 9f685826-6130-7644-9a65-f1caa281a29b

--- client/examples/SlotMachine/build/update_config_h.sh
+++ client/examples/SlotMachine/build/update_config_h.sh
@@ -11,66 +11,66 @@ echo "param 7 : $7"
 echo "param 8 : $8"
 
 # check template file
-if [ ! -f $1 ]; then
-    echo "Error, TEMPLATE_FILE not specified"
-    exit 1
-fi
-TEMPLATE_FILE="$1"
+#if [ ! -f $1 ]; then
+#    echo "Error, TEMPLATE_FILE not specified"
+#    exit 1
+#fi
+#TEMPLATE_FILE="$1"
 echo "TEMPLATE_FILE = ${TEMPLATE_FILE}"
 
 # check config file
-if [ -z $2 ]; then
-    echo "Error, CONFIG_FILE not specified"
-    exit 1
-fi
+#if [ -z $2 ]; then
+#    echo "Error, CONFIG_FILE not specified"
+#    exit 1
+#fi
 CONFIG_FILE="$2"
 echo "CONFIG_FILE = ${CONFIG_FILE}"
 
 
-if [ -z $3 ]; then
-    echo "Error, ENGINE_VER_MAJOR not specified"
-    exit 1
-fi
+#if [ -z $3 ]; then
+#    echo "Error, ENGINE_VER_MAJOR not specified"
+#    exit 1
+#fi
 ENGINE_VER_MAJOR="$3"
 echo "ENGINE_VER_MAJOR = ${ENGINE_VER_MAJOR}"
 
 
-if [ -z $4 ]; then
-    echo "Error, ENGINE_VER_MINOR not specified"
-    exit 1
-fi
+#if [ -z $4 ]; then
+#    echo "Error, ENGINE_VER_MINOR not specified"
+#    exit 1
+#fi
 ENGINE_VER_MINOR="$4"
 echo "ENGINE_VER_MINOR = ${ENGINE_VER_MINOR}"
 
 
-if [ -z $5 ]; then
-    echo "Error, ENGINE_VER_PATCH not specified"
-    exit 1
-fi
+#if [ -z $5 ]; then
+#    echo "Error, ENGINE_VER_PATCH not specified"
+#    exit 1
+#fi
 ENGINE_VER_PATCH="$5"
 echo "ENGINE_VER_PATCH = ${ENGINE_VER_PATCH}"
 
 
-if [ -z $6 ]; then
-    echo "Error, CASINO_VER_MAJOR not specified"
-    exit 1
-fi
+#if [ -z $6 ]; then
+#    echo "Error, CASINO_VER_MAJOR not specified"
+#    exit 1
+#fi
 CASINO_VER_MAJOR="$6"
 echo "CASINO_VER_MAJOR = ${CASINO_VER_MAJOR}"
 
 
-if [ -z $7 ]; then
-    echo "Error, CASINO_VER_MINOR not specified"
-    exit 1
-fi
+#if [ -z $7 ]; then
+#    echo "Error, CASINO_VER_MINOR not specified"
+#    exit 1
+#fi
 CASINO_VER_MINOR="$7"
 echo "CASINO_VER_MINOR = ${CASINO_VER_MINOR}"
 
 
-if [ -z $8 ]; then
-    echo "Error, CASINO_VER_PATCH not specified"
-    exit 1
-fi
+#if [ -z $8 ]; then
+#    echo "Error, CASINO_VER_PATCH not specified"
+#    exit 1
+#fi
 CASINO_VER_PATCH="$8"
 echo "CASINO_VER_PATCH = ${CASINO_VER_PATCH}"
 
